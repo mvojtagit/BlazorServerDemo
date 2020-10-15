c4674632192ecbf913130ab860256dd09b54a9d6
mvojtagit <57258474+mvojtagit@users.noreply.github.com>
2020-10-15 17:44:01 +0200

    Add project files.

---
+++ BlazorServerDemoApp/Models/OrderUpdateModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerDemoApp.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Name for narudzba")]
+        [MaxLength(20, ErrorMessage = "Max broj znakova je 20")]
+        [MinLength(3, ErrorMessage = "Unesi barem tri znaka")]
+        [Required]
+        public string OrderName { get; set; }
+
+    }
+}
